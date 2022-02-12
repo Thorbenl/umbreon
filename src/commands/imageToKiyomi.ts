305b76e8bd1fbeb716114f49ad6e3b0c6b8b5993
Thorben Luepkes <turbzcoding@gmail.com>
2022-02-12 18:28:41 +0100

    1) wip: docker 2) form data to kiyomi, and sending discord id

--- src/commands/imageToKiyomi.ts
+++ src/commands/imageToKiyomi.ts
@@ -1,14 +1,21 @@
 import { Command } from "../interfaces/Command";
 import {SlashCommandBuilder} from "@discordjs/builders";
 const axios = require('axios').default;
+const NodeFormData = require('form-data');
 
-async function sendToKiyomi(url: string) {
+async function sendToKiyomi(url: string, discordId: string) {
+    const kiyomiURL = "https://kiyomi.io/api/image/upload"
+    const formData = new NodeFormData();
+    formData.append('file', url);
+    formData.append('discordId', discordId);
+    const formDataHeaders = formData.getHeaders();
     try {
-        const response = await axios.post('https://webhook.site/b3424aba-f228-4a52-a62d-b25e42914cba?',
-        {
-            url: url
-        })
-        return response
+        return await axios.post(kiyomiURL, formData, {
+            headers: {
+                "Authorization": "KIYOMI_7c9ff7152ad4dbdba00024b810ae08377d2d8f3dbd7aa442f74a16aca2b7a82b",
+                ...formDataHeaders
+            },
+        });
     } catch (error) {
         console.error(error);
     }
@@ -26,7 +33,10 @@ export const imageToKiyomi: Command = {
         ),
     run: async (interaction) => {
         await interaction.deferReply()
-        const response = await sendToKiyomi(interaction.options.getString("image_url", true))
+        const response = await sendToKiyomi(
+            interaction.options.getString("image_url", true),
+            interaction.user.id
+        )
         console.log(response)
         await interaction.followUp(`${interaction.user.username} has sent an image to Kiyomi`)
     },
